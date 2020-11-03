38455990ef4a26487c14837584ba7dc268c0c4cf
Adebisi Ahmed <adebisi.ahmed10@gmail.com>
2020-11-04 00:59:38 +0100

    use KUTE for animation

--- src/Hooks/useShapeAnimation.js
+++ src/Hooks/useShapeAnimation.js
@@ -1,15 +1,15 @@
 import * as React from "react";
+import KUTE from "kute.js";
 
 const useShapeAnimation = (length) => {
   const pathRef = React.useRef();
-  const tl = window.gsap.timeline({ onComplete: function () {} });
 
   React.useEffect(() => {
     const path = pathRef.current;
     if (!path) return;
-    tl.set(path, { drawSVG: "0% 0%", opacity: 0 });
-    tl.to(path, 2, { drawSVG: "100% 0", opacity: 1, delay: 0.4 });
-  }, [tl, length]);
+    KUTE.set(path, { drawSVG: "0% 0%", opacity: 0 });
+    KUTE.to(path, 2, { drawSVG: "100% 0", opacity: 1, delay: 0.4 });
+  }, [length]);
 
   return [pathRef];
 };
